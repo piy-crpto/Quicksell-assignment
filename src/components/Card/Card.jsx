e5ac389570eea2510cfd868eed8b5eaf002379ef
Piyush Yadav <85944872+piy-crpto@users.noreply.github.com>
2023-10-30 11:06:36 +0530

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,18 +7,18 @@ const Card = ({id, title, tag, status}) => {
         <div className="cardHeading flex-sb">
             <span style={{textTransform : "uppercase"}} className='color-grey'>{id}</span>
             <div className="imageContainer relative" style={{ width : "30px", height : "30px"}}>
-                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="UserImage" />
+                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://plus.unsplash.com/premium_photo-1664298528358-790433ba0815?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="UserImage" />
                 <div className="showStatus"></div>
             </div>
         </div>
-        <div className="cardTitle" style={{fontWeight : 200}} >
+        <div className="cardTitle" style={{fontWeight : 150}} >
             <p>{title}</p>
         </div>
         <div className="cardTags">
         <div className="tags color-grey"> ... </div>
             {
                 tag?.map((elem, index) => {
-                    return <div key={index} className="tags color-grey"> <span>•</span> {elem}</div>
+                    return <div key={index} className="tags color-green"> <span>•</span> {elem}</div>
                 })
             }
         </div>
