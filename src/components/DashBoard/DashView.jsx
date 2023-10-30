0a0d02cbacad19790c3986eb2efec969a6634a78
Piyush Yadav <85944872+piy-crpto@users.noreply.github.com>
2023-10-30 11:07:12 +0530

    Update DashView.jsx

--- src/components/DashBoard/DashView.jsx
+++ src/components/DashBoard/DashView.jsx
@@ -33,7 +33,7 @@ const DashView = () => {
                               height: "100%",
                               borderRadius: "50%",
                             }}
-                            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80"
+                            src="https://plus.unsplash.com/premium_photo-1664298528358-790433ba0815?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="UserImage"
                           />
                         </div>
